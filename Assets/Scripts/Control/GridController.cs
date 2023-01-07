48583b183d20b1ee267c788a6e87027fdf8d680b
Dorian Roques <dorian-roques@orange.fr>
2023-01-07 12:59:20 +0100

    Implement grid squeletton in control and model

---
+++ Assets/Scripts/Control/GridController.cs
@@ -0,0 +1,9 @@
+public class GridController
+{
+    private Grid grid;
+
+    public GridController()
+    {
+        grid = new Grid();
+    }
+}
---
+++ Assets/Scripts/Control/TileController.cs
@@ -0,0 +1,4 @@
+public abstract class TileController
+{
+    private Tile tile;
+}
