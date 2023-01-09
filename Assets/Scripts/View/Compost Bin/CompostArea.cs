1d4e3057a2732c5ab65dcad0b2c6daf82b16fabe
Dorian Roques <dorian-roques@orange.fr>
2023-01-09 11:02:57 +0100

    Clean compost code

--- Assets/Scripts/View/Compost Bin/CompostArea.cs
+++ Assets/Scripts/View/Compost Bin/CompostArea.cs
@@ -1,5 +1,4 @@
 using System;
-using System.Collections;
 using UnityEngine;
 
 public class CompostArea : MonoBehaviour
@@ -10,64 +9,11 @@ public class CompostArea : MonoBehaviour
     [SerializeField]
     private WastesContainer wastesContainer;
 
-    private CompostStates compostState;
-
     public event Action OnWasteReceivingComplete;
 
     protected void Start()
     {
         bin.OnWasteReceivingComplete += Handle_OnWasteReceivingComplete;
-
-        // StartCoroutine(Test());
-    }
-
-    private IEnumerator Test()
-    {
-        yield return new WaitForSeconds(1);
-        // Debug.Log("OK 1");
-
-        OpenBin();
-        AddWaste(0, PlantTypes.Wheat, 6);
-        AddWaste(1, PlantTypes.Corn, 4);
-        AddWaste(2, PlantTypes.Pumpkin, 2);
-        AddWaste(3, PlantTypes.Chilli, 3);
-
-        yield return new WaitForSeconds(3);
-        // Debug.Log("OK 2");
-
-        AcceptWastes();
-        
-        yield return new WaitForSeconds(3);
-        // Debug.Log("OK 3");
-        
-        OpenBin();
-        AddWaste(0, PlantTypes.Wheat, 6);
-        AddWaste(1, PlantTypes.Corn, 4);
-        AddWaste(2, PlantTypes.Pumpkin, 2);
-        AddWaste(3, PlantTypes.Chilli, 3);
-        
-        yield return new WaitForSeconds(3);
-        
-        AcceptWastes();
-    }
-
-    protected void Update()
-    {
-        switch (compostState)
-        {
-            case CompostStates.CLOSED:
-
-                break;
-            case CompostStates.OPENING:
-
-                break;
-            case CompostStates.OPEN:
-
-                break;
-            case CompostStates.CLOSING:
-
-                break;
-        }
     }
 
     public void AddWaste(int slotIndex, PlantTypes plantType, int wasteCount)
